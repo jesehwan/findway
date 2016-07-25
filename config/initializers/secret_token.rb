a3089364d3995e67bf47a412ec32a227b2c304df
jesehwan <jesehwan@gmail.com>
2016-07-25 05:38:50 +0000

    첫 커밋

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Findway::Application.config.secret_key_base = '2fa8773ee5992fb2fb15395bdc064a607b6768882bfc7911371cff3a1444d06a16caedd4aa6d415cdd9ac0360f3e57fd0df866ef9f43f77e2390d5e17a1197bc'
