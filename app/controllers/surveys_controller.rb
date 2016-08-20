e3b4acc637757188d36f13522d354ac715aed05a
jesehwan <jesehwan@gmail.com>
2016-08-20 19:12:10 +0000

    스탭1페이지 완성/설문페이지 컨트롤러수정

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,11 +5,28 @@ class SurveysController < ApplicationController
 	end
 
   def next
-		@check_answers = Answer.find(params[:check])
+		@user_info = User.find(1)
 
+		if params[:check].nil? 
 
+			flash[:alert] = "1개이상 선택해야합니다"
+			redirect_to :back
 	
-		
+		else
+			@check_answers = Answer.find(params[:check])
+			id = 0
+			id = @check_answers.first.question_id
+			@check_answers.each do |answer|
+				p = Survey.new
+				p.user_id = @user_info.id
+				p.try_number = @user_info.try + 1
+				p.question_id = answer.question_id
+				p.answer_id = answer.id
+				p.type_id = answer.type_id
+				p.save
+			end
+			redirect_to "/surveys/paper/#{id+1}" 
+		end	
   end
 
   def finish
